33a6722dbd5199c13424d630b456e6f705187151
tj-khalil <960809@pdsb.net>
2019-10-21 10:43:24 -0400

    1
    1

--- JS.js
+++ JS.js
@@ -1,3 +1,33 @@
-function display() {
-   alert("Hello");
+function Mathsss(){
+
+const math1 = parseInt(prompt('Please enter an angle between 1 and 90'));
+
+if (math1 > 90){
+	return alert('Number should be between 1 and 90');
 }
+
+else if (math1 <= 0){
+	return alert('Number should be between 1 and 90');
+}
+
+else if (math1 == ''){
+	return alert('Number should be between 1 and 90');
+}
+
+const math2 = parseInt(prompt('Please enter another angle between 1 and 90'));
+
+if (math2 > 90){
+	return alert('Number should be between 1 and 90');
+}
+
+else if (math2 <= 0){
+	return alert('Number should be between 1 and 90');
+}
+
+else if (math1 == ''){
+	return alert('Number should be between 1 and 90');
+}
+
+return alert('Your remaining angle is ' + (180 -(math1 + math2)));
+}
+
