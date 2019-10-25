3bff51dd93ff3291fadd32dfb5accb18c5dc7ca9
tj-khalil <960809@pdsb.net>
2019-10-25 11:01:44 -0400

    fin

--- JS.js
+++ JS.js
@@ -1,33 +1,48 @@
 function Mathsss(){
 
-const math1 = parseInt(prompt('Please enter an angle between 1 and 90'));
+	const math1 = parseInt(prompt('Please enter an angle between 1 and 90'));
 
-if (math1 > 90){
-	return alert('Number should be between 1 and 90');
+	if (math1 < 90 && math1 > 0){
+	
+		const math2 = parseInt(prompt('Please enter another angle between 1 and 90'));
+	
+		if (math2 < 90 && math2 > 0){
+
+			alert('Your remaining angle is ' + (180 -(math1 + math2)));
+			
+		} else {
+			math2 = alert('Wrong! Please enter another angle between 1 and 90');
+		}
+	} else {
+		math1 = alert('Wrong! Please enter an angle between 1 and 90');
+	}
 }
 
-else if (math1 <= 0){
-	return alert('Number should be between 1 and 90');
+function Cent(){
+
+	const year = parseInt(prompt('Please enter a year.'));
+
+	if (year <= 0){
+		alert('Please enter a valid year.');
+	}
+
+	else {
+	alert('This year is in century '+ Math.ceil(year / 100))
+	}
 }
 
-else if (math1 == ''){
-	return alert('Number should be between 1 and 90');
+function counter(){
+var x = 0;
+	while (x <= 99){
+		x = x + 1;
+		if(x % 4 == 0 && x % 10 == 0) {
+			alert('fourten');
+		}else if (x % 10 == 0){
+			alert('ten');
+		}else if (x % 4 == 0){
+			alert('four');
+		}else {
+			alert(x);
+		}
+	}
 }
-
-const math2 = parseInt(prompt('Please enter another angle between 1 and 90'));
-
-if (math2 > 90){
-	return alert('Number should be between 1 and 90');
-}
-
-else if (math2 <= 0){
-	return alert('Number should be between 1 and 90');
-}
-
-else if (math1 == ''){
-	return alert('Number should be between 1 and 90');
-}
-
-return alert('Your remaining angle is ' + (180 -(math1 + math2)));
-}
-
